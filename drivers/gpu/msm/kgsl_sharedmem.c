92506c898e6eeecb924f850b9e31becdb2dde018
Aid Farhan Maarof <aidfarhan@gmail.com>
2014-04-12 16:27:15 +0800

    update GPU drivers from xzaosp
    Change-Id: Idd80c4eb351916665f2bad3023f57374a6f18a37

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2007-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2002,2007-2012,2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -73,12 +73,12 @@ static struct kgsl_process_private *
 _get_priv_from_kobj(struct kobject *kobj)
 {
 	struct kgsl_process_private *private;
-	unsigned long name;
+	unsigned int name;
 
 	if (!kobj)
 		return NULL;
 
-	if (sscanf(kobj->name, "%ld", &name) != 1)
+	if (kstrtou32(kobj->name, 0, &name))
 		return NULL;
 
 	list_for_each_entry(private, &kgsl_driver.process_list, list) {
@@ -334,11 +334,6 @@ static int kgsl_page_alloc_vmfault(struct kgsl_memdesc *memdesc,
 
 			page = nth_page(page, pgoff);
 
-			if (!memdesc->faulted[pgoff]) {
-				memdesc->faulted[pgoff] = 1;
-				__inc_zone_page_state(page, NR_FILE_PAGES);
-			}
-
 			get_page(page);
 			vmf->page = page;
 
@@ -369,28 +364,12 @@ static void kgsl_page_alloc_free(struct kgsl_memdesc *memdesc)
 		vunmap(memdesc->hostptr);
 		kgsl_driver.stats.vmalloc -= memdesc->size;
 	}
-	if (memdesc->sg) {
-		size_t pgindex = 0;
+	if (memdesc->sg)
 		for_each_sg(memdesc->sg, sg, sglen, i){
-			struct page *page;
-			unsigned int j, npages;
 			if (sg->length == 0)
 				break;
-
-			page = sg_page(sg);
-			npages = sg->length >> PAGE_SHIFT;
-
-			for (j = 0; j < npages; ++j) {
-				if (memdesc->faulted[pgindex]) {
-					__dec_zone_page_state(page + j,
-							      NR_FILE_PAGES);
-					memdesc->faulted[pgindex] = 0;
-				}
-				++pgindex;
-			}
-			__free_pages(page, get_order(sg->length));
+			__free_pages(sg_page(sg), get_order(sg->length));
 		}
-	}
 }
 
 static int kgsl_contiguous_vmflags(struct kgsl_memdesc *memdesc)
@@ -575,13 +554,6 @@ _kgsl_sharedmem_page_alloc(struct kgsl_memdesc *memdesc,
 
 	memdesc->pagetable = pagetable;
 	memdesc->ops = &kgsl_page_alloc_ops;
-
-	memdesc->faulted = kzalloc(sglen_alloc*sizeof(int), GFP_KERNEL);
-
-	if (memdesc->faulted == NULL) {
-		ret = -ENOMEM;
-		goto done;
-	}
 
 	memdesc->sglen_alloc = sglen_alloc;
 	memdesc->sg = kgsl_sg_alloc(memdesc->sglen_alloc);
@@ -803,7 +775,6 @@ void kgsl_sharedmem_free(struct kgsl_memdesc *memdesc)
 		memdesc->ops->free(memdesc);
 
 	kgsl_sg_free(memdesc->sg, memdesc->sglen_alloc);
-	kfree(memdesc->faulted);
 
 	memset(memdesc, 0, sizeof(*memdesc));
 }
