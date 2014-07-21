6fb09c286ded0011ae2f65d55c494841f35aca1c
root <root@schorsch.funke>
2014-07-21 05:14:51 +0200

    Revert "Another fix for lz4_decompress.c"
    This reverts commit 7b948fbbc705cbdac1adf8dc2fb55d1bcf7e59a3.

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -106,7 +106,6 @@ static int lz4_uncompress(const char *source, char *dest, int osize)
 		/* get matchlength */
 		length = token & ML_MASK;
 		if (length == ML_MASK) {
-			size_t len;
 			for (; *ip == 255; length += 255)
 				len = *ip++;
 			if (unlikely(length > (size_t)(length + len)))
